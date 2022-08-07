e3c31aa2063060308df0e4216c7f9d994fd2c3ca
alexz330 <alexislc330@gmail.com>
2022-08-07 10:56:46 -0600

    agregando servicios

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsController } from './controlers/products.controller';
 import { CategoriesController } from './controlers/categories.controller';
+import { OrderController } from './controlers/order.controller';
+import { UserController } from './controlers/user.controller';
+import { CustomerController } from './controlers/customer.controller';
+import { BrandController } from './controlers/brand.controller';
+import { ProductsService } from './services/products.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, ProductsController, CategoriesController],
-  providers: [AppService],
+  controllers: [AppController, ProductsController, CategoriesController, OrderController, UserController, CustomerController, BrandController],
+  providers: [AppService, ProductsService],
 })
 export class AppModule {}
--- src/controlers/products.controller.ts
+++ src/controlers/products.controller.ts
@@ -1,9 +1,40 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  Post,
+  Body,
+  HttpStatus,
+  HttpCode,
+  Param,
+  Put,
+} from '@nestjs/common';
+
+import { ProductsService } from './../services/products.service';
 
 @Controller('products')
 export class ProductsController {
+  constructor(private productsSevice: ProductsService) {}
+
+  @Get(':productId')
+  @HttpCode(HttpStatus.ACCEPTED)
+  getOne(@Param('productId') productId: number) {
+    return this.productsSevice.findOne(productId);
+  }
+
   @Get()
-  getProducts(@Query('limit') limit: number, @Query('offset') offset: number) {
-    return `products: \ limit => ${limit} offset => ${offset}`;
+  getProducts() {
+    return this.productsSevice.findAll();
+  }
+
+  @Post()
+  create(@Body() payload: any) {
+    return this.productsSevice.create(payload);
+  }
+  @Put(':id')
+  update(@Param('id') id: number, @Body() payload: any) {
+    console.log(id, payload);
+    
+    return this.productsSevice.update(id, payload);
   }
 }
